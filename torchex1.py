6043ec61bce8ae14309a609ab455140f20e67a30
hanuengine <32562017+hanuengine@users.noreply.github.com>
2018-01-09 20:49:45 +0900

    Update torchex1.py

--- torchex1.py
+++ torchex1.py
@@ -1,7 +1,8 @@
-import torch
-x = torch.rand(2,2)
-y = torch.rand(2,2)
+import torch #torch import
+x = torch.rand(2,2) #random한 2x2행렬구성
+y = torch.rand(2,2) #random한 2x2행렬구성
 
+#행렬 x,y를 print하고 두 행렬을 더함
 print(x)
 print(y)
 print(x+y)
